b5759dcfd7c7b8f182a4463bd0b6e537ddf41875
Roger Lew <rogerlew@gmail.com>
2022-06-16 02:24:35 +0000

    ermit cleanup

--- var/www/nickTest/nickTest.php
+++
@@ -1,6 +0,0 @@
-<?php echo "<p>Hello"; ?>
-
-<?php echo "<p>Remote addr=" . $_SERVER['REMOTE_ADDR']; ?>
-<?php echo "<p>Remote user=" . $_SERVER['REMOTE_USER']; ?>
-<?php echo "<p>Remote port=" . $_SERVER['REMOTE_PORT']; ?>
-
